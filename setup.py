b995c3e2663e56af78e2b2adff0fbe2f88aee608
Invoker <chiqingjun@gmail.com>
2019-05-01 16:41:04 +0800

    make the project installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),       # find package directories automatically
+    include_package_data=True,      # include other files listed in MANIFEST.in
+    zip_safe=False,
+    install_requires=[
+        'flask', 'flask-sqlalchemy', 'flask-migrate', 'psycopg2', 'flask-login'
+    ]
+)
